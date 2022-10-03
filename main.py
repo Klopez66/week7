e855c4fa409c5376e89653b3a7025653a6420d55
Katherine Lopez <113552151+Klopez66@users.noreply.github.com>
2022-10-03 19:41:55 +0000

    defined variables

---
+++ main.py
@@ -0,0 +1,36 @@
+# fork the repl.it and create a new git hub repo entitled 
+#Comparison operators
+# Logical operators
+# Decision making
+# loops(for loops, while loops, range, enumerator)
+# min/max practice
+# Random in python
+# List comprehension
+#importing modules
+#work on creating and understanding functions
+
+# from decision_making import decisions
+# from comparison_operators import comparison
+# from dictionaries_loops import dictionaries_loops 
+# from enumerators import enumerators 
+# from comprehension import comprehension
+# from operators import operators 
+from loops import loops 
+# from min_max import min_max
+# from random import random
+# from ranges import ranges 
+# from while_loops import while_loops 
+# from zip import zip
+
+# decisions()
+# comparison()
+# dictionaries_loops()
+# enumerators()
+# comprehension()
+# operators()
+loops()
+# min_max()
+# random()
+# ranges()
+# while_loops()
+# zip()
